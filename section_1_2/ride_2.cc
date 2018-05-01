aefcaf13e99d751b9adcbf0e4ac7ad923dc71043
Staphany Park <stapark008@gmail.com>
2018-04-30 17:12:14 -0700

    ride 2nd try. [1.2]

---
+++ section_1_2/ride_2.cc
@@ -0,0 +1,30 @@
+/*
+ID: stapark1
+LANG: C++11
+TASK: ride
+*/
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int hash(std::string name) {
+  int result = 1;
+  for (const char letter : name) {
+    result *= (letter - 'A' + 1);
+  }
+  return result % 47;
+}
+
+int main() {
+  std::ifstream input("ride.in");
+  std::ofstream output("ride.out");
+
+  std::string comet, group;
+  input >> comet >> group;
+
+  std::string result = hash(comet) == hash(group) ? "GO" : "STAY";
+  output << result << std::endl;
+
+  return 0;
+}
