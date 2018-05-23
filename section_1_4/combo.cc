b96ecadb96ac5afc2416acb41b6b12f25b63e191
Staphany Park <stapark008@gmail.com>
2018-05-22 20:34:57 -0700

    Revise combo.

--- section_1_4/combo.cc
+++ section_1_4/combo.cc
@@ -26,16 +26,16 @@ int main() {
   std::unordered_set<int> valid_combos;
   std::array<std::unordered_set<int>, 3> valid_positions;
 
-  int position;
   for (int official_combo = 0; official_combo < 2; ++official_combo) {
     for (int i = 0; i < 3; ++i) {
+      int position;
       input >> position;
       valid_positions[i] = valid_positions_for(position, dial_max);
     }
     for (const auto p1 : valid_positions[0]) {
       for (const auto p2 : valid_positions[1]) {
         for (const auto p3 : valid_positions[2]) {
-          valid_combos.insert((100 * p1) + (10 * p2) + p3);
+          valid_combos.insert((10000 * p1) + (100 * p2) + p3);
         }
       }
     }
