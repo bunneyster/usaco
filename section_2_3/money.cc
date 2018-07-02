4ba701c97cf664a4389b2acdeb85da0dcba66bbc
Staphany Park <stapark008@gmail.com>
2018-07-01 23:24:22 -0700

    Revise money.

--- section_2_3/money.cc
+++ section_2_3/money.cc
@@ -15,25 +15,23 @@ int main() {
   input >> numTypes >> totalValue;
 
   std::vector<int> coinTypes = {0};
-  for (std::size_t type = 0; type < numTypes; ++type) {
-    int coinType;
-    input >> coinType;
+  int coinType;
+  while (input >> coinType)
     coinTypes.emplace_back(coinType);
-  }
 
   // Element [i][j] is the number of ways to achieve a sum of j using the first
   // i coin types.
   std::vector<std::vector<long long>> solutions;
-  solutions.emplace_back(std::vector<long long> {1});
-  solutions.back().resize(totalValue + 1);
+  solutions.emplace_back(totalValue + 1, 0);
+  solutions[0][0] = 1;
 
-  for (std::size_t i = 1; i <= numTypes; ++i) {
-    solutions.emplace_back(std::vector<long long> {1});
+  for (int i = 1; i <= numTypes; ++i) {
+    solutions.emplace_back(totalValue + 1, 0);
+    solutions[i][0] = 1;
     for (int j = 1; j <= totalValue; ++j) {
-      long long useNone = solutions[i - 1][j];
-      long long useSome = (j >= coinTypes[i]) ?
-          solutions[i][j - coinTypes[i]] : 0;
-      solutions.back().emplace_back(useNone + useSome);
+      solutions[i][j] = solutions[i - 1][j];
+      if (j >= coinTypes[i])
+          solutions[i][j] += solutions[i][j - coinTypes[i]];
     }
   }
 
